f3946e590d9530786cf0775eec4c3d51c2d70746
je-ni <jenniferolibie@gmail.com>
2018-12-12 00:39:09 +0100

    modified Schema structure

--- server/joiSchema/user.js
+++ server/joiSchema/user.js
@@ -1,18 +1,23 @@
 import Joi from 'joi';
 
-const schema = Joi.object().keys({
-  firstname: Joi.string().required(),
-  lastname: Joi.string().required(),
-  othernames: Joi.string(),
-  username: Joi.string().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(8).required(),
-});
-
-const validate = (value) => {
-  const result = Joi.validate(value, schema);
-  if (result === null) return false;
-  return result.error.details[0].message;
+const Schema = {
+  validate(value, schema) {
+    const result = Joi.validate(value, schema);
+    if (result.error == null) return false;
+    return result.error.details[0].message;
+  },
+  signup: Joi.object().keys({
+    firstname: Joi.string().required(),
+    lastname: Joi.string().required(),
+    othernames: Joi.string().allow(''),
+    username: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+  login: Joi.object().keys({
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  })
 };
 
-export default validate;
+export default Schema;
