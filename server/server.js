08fba85526b8327962f423136e8b868fe65efe27
je-ni <jenniferolibie@gmail.com>
2018-12-11 23:25:12 +0100

    exported server for testing

--- server/server.js
+++ server/server.js
@@ -4,3 +4,5 @@ import app from './app'; // The express app we just created
 const port = parseInt(process.env.PORT, 10) || 8000;
 
 app.listen(port, () => console.log(`App started on port ${port}`));
+
+module.exports = app;
