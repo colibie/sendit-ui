b9ac21c854b69577f972686652cc1cd628ffb88d
je-ni <jenniferolibie@gmail.com>
2018-12-13 16:29:28 +0100

    added by by id route

--- server/router/parcel.js
+++ server/router/parcel.js
@@ -3,8 +3,9 @@ import parcel from '../controller/parcel';
 
 const router = Router();
 
-router.post('/create', parcel.create);
+router.post('/', parcel.create);
 router.get('/', parcel.getAll);
+router.get('/:parcelId', parcel.getById);
 // router.post('/auth/login', user.login);
 
 export default router;
