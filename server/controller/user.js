385cfcb06eed55c0882065039c6a868c45154097
je-ni <jenniferolibie@gmail.com>
2018-12-13 14:23:27 +0100

    modified res.json format

--- server/controller/user.js
+++ server/controller/user.js
@@ -10,18 +10,18 @@ const User = {
   async create(req, res) {
     // validate req.body
     const valid = joi.validate(req.body, joi.signup);
-    if (valid) return res.status(409).json(valid);
+    if (valid) return res.status(409).json({ status: 409, message: valid });
 
     const password = hash(req.body.password);
     
     try {
       // check if username exists already
       let exists = await doesExist('username', req.body.username);
-      if (exists) return res.status(409).send({ message: 'Username already exists' });     
+      if (exists) return res.status(409).json({ status: 409, message: 'Username already exists' });     
       
       // check if email exists already
       exists = await doesExist('email', req.body.email);
-      if (exists) return res.status(409).send({ message: 'Email already exists' }); 
+      if (exists) return res.status(409).json({ status: 409, message: 'Email already exists' }); 
       
       // continue if not duplicates
       const text = `INSERT INTO
@@ -42,18 +42,18 @@ const User = {
       ];
       try {
         const { rows } = await db.query(text, values);
-        return res.status(201).send(rows[0]);
+        return res.status(201).json({ status: 201, message: rows[0] });
       } catch (error) {
-        return res.status(500).send(error);
+        return res.status(500).json({ status: 500, message: error });
       }
     } catch (error) {
-      return res.status(500).send(error);
+      return res.status(500).json({ status: 500, message: error });
     }
   },
   async login(req, res) {
     // validate req.body
     const valid = joi.validate(req.body, joi.login);
-    if (valid) return res.status(409).json(valid);
+    if (valid) return res.status(409).json({ status: 409, message: valid });
     
     const comparePassword = hash(req.body.password);
     const text = 'SELECT * FROM users WHERE email = $1 AND password = $2';
@@ -61,28 +61,29 @@ const User = {
     try {
       const { rows } = await db.query(text, values);
       
-      if (!rows[0]) return res.status(401).send('Email/Password Incorrect');
+      if (!rows[0]) return res.status(401).json({ status: 401, message: 'Email/Password Incorrect' });
       console.log(auth.setToken({ id: rows[0].id }));
-      return res.status(200).send({
+      return res.status(200).json({
+        status: 500,
         isAdmin: rows[0].isadmin, 
         token: auth.setToken({ id: rows[0].id }), 
         result: rows[0] 
       });
     } catch (error) {
-      return res.status(500).send(error);
+      return res.status(500).json({ status: 500, message: error });
     }
   },
   async getAll(req, res) {
     const access = auth.adminAuth(req);
-    if (!access) return res.status(504).send('user access denied');
+    if (!access) return res.status(504).json({ status: 504, message: 'user access denied' });
 
     const text = 'SELECT * FROM users';
     try {
       const { rows } = await db.query(text, []);
-      if (!rows[0]) return res.status(200).send({ message: 'No existing users' });
-      return res.status(200).send(rows);
+      if (!rows[0]) return res.status(200).json({ status: 200, message: 'No existing users' });
+      return res.status(200).json({ status: 200, message: rows });
     } catch (error) {
-      return res.status(500).send(error);
+      return res.status(500).json({ status: 500, message: error });
     }
   }
 };
