c0b2a9dbb1bef7db7e3bde8f5356dc55a943287b
je-ni <jenniferolibie@gmail.com>
2018-12-13 14:25:03 +0100

    added parcel route

--- server/router/index.js
+++ server/router/index.js
@@ -1,5 +1,7 @@
 import userRouter from './user';
+import parcelRouter from './parcel';
 
 export default (app) => {
   app.use('/api/v1/users', userRouter);
+  app.use('/api/v1/parcels', parcelRouter);
 };
