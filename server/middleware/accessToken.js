60d3899d17ed1109851bc89d5af3b2b600972496
je-ni <jenniferolibie@gmail.com>
2018-12-18 17:51:04 +0100

    added admin access tokens

--- server/middleware/accessToken.js
+++ server/middleware/accessToken.js
@@ -1,9 +1,18 @@
 import jwt from 'jsonwebtoken';
 import env from './ENVdevelopment.json';
+import db from '../controller/index';
 
 export const setToken = (payload) => {
   const tokenTime = 60 * 60 * 24 * 30; // expires in 30 days
   const token = jwt.sign(payload, env.secret, {
+    expiresIn: tokenTime
+  });
+  return token;
+};
+
+export const setAdminToken = (payload) => {
+  const tokenTime = 60 * 60 * 24 * 30; // expires in 30 days
+  const token = jwt.sign(payload, env.adminSecret, {
     expiresIn: tokenTime
   });
   return token;
@@ -17,6 +26,14 @@ export const verifyToken = (accessToken) => {
   return output;
 };
 
+export const verifyAdminToken = (accessToken) => {
+  let output;
+  jwt.verify(accessToken, env.adminSecret, (err) => {
+    output = !err;
+  });
+  return output;
+};
+
 export const userAuth = (req) => {
   const accessToken = req.body.token || req.query.token || req.headers['x-access-token'];
   if (!accessToken) return false;
@@ -25,12 +42,19 @@ export const userAuth = (req) => {
   return true;
 };
 
-export const adminAuth = (req) => {
+export const adminAuth = async (req) => {
   const accessToken = req.body.token || req.query.token || req.headers['x-access-token'];
   if (!accessToken) return false;
-  const isTokenValid = verifyToken(accessToken);
+  const isTokenValid = verifyAdminToken(accessToken);
   if (isTokenValid === false) return false;
-  const isAdmin = req.body.isAdmin || req.params.isAdmin || req.query.isAdmin;
-  if (!isAdmin) return false;
+
+  // check if admin id exists in db
+  const text = 'SELECT * FROM admins WHERE id = $1';
+  try {
+    const { rows } = await db.query(text, [req.params.adminId]);
+    if (!rows[0]) return false;
+  } catch (error) {
+    return console.log(error);
+  }
   return true;
 };
