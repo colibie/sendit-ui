48d78874975353207a5b25ecfb8226216a2faed3
je-ni <jenniferolibie@gmail.com>
2018-12-21 00:27:20 +0100

    add user email in parcel fetch

--- server/controller/parcel.js
+++ server/controller/parcel.js
@@ -1,30 +1,27 @@
 import uuidv4 from 'uuid/v4';
 import db from './index';
-import doesExist from '../middleware/doesExist';
 import joi from '../joiSchema/parcel';
-import * as auth from '../middleware/accessToken';
 import mail from '../middleware/mailer';
+
+let text = `SELECT p.id, p.placedby, p.weight, p.status, p.weightmetric, p.senton, p.currentlocation, p.sentfrom, p.sentto, p.description,
+u.username, u.email FROM parcels p 
+INNER JOIN users u
+ON p.placedby = u.id 
+WHERE p.id = $1`;
 
 const Parcel = {
   async create(req, res) {
-    const access = auth.userAuth(req);
-    if (!access) return res.status(401).send({ status: 401, error: 'user access denied' });
-
     const valid = joi.validate(req.body, joi.create);
     if (valid) return res.status(422).json({ status: 422, error: valid });
 
     try {
-      // check if placedBy is valid userid
-      const exists = await doesExist('id', req.body.placedby);
-      if (!exists) return res.status(422).send({ status: 422, error: 'user id is not valid' });
-
       const text = `INSERT INTO
       parcels(id, placedby, weight, weightmetric, senton, currentlocation, sentfrom, sentto, description)
       VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
       returning *`;
       const values = [
         uuidv4(),
-        req.body.placedby,
+        req.userData.id,
         req.body.weight,
         req.body.weightmetric,
         new Date(),
@@ -50,10 +47,10 @@ const Parcel = {
     }
   },
   async getAll(req, res) {
-    const access = auth.adminAuth(req);
-    if (!access) return res.status(401).json({ status: 401, error: 'user access denied' });
-
-    const text = 'SELECT * FROM parcels';
+    const text = `SELECT p.id, p.placedby, p.weight, p.weightmetric, p.senton, p.currentlocation, p.sentfrom, p.sentto, p.description,
+    u.username, u.email FROM parcels p 
+    INNER JOIN users u
+    ON p.placedby = u.id `;
     try {
       const { rows } = await db.query(text, []);
       return res.status(200).json({ status: 200, data: rows });
@@ -62,16 +59,10 @@ const Parcel = {
     }
   },
   async getById(req, res) {
-    const userAccess = auth.userAuth(req);
-    const adminAccess = auth.adminAuth(req);
-    if (!(userAccess || adminAccess)) return res.status(401).json({ status: 401, error: 'user access denied' });
-
-    const text = 'SELECT * FROM parcels where id = $1';
     try {
       const { rows } = await db.query(text, [req.params.parcelId]);
-      if (userAccess && !adminAccess) {
-        if (!rows[0]) return res.status(200).json({ status: 200, data: rows });
-        if (rows[0].placedby !== req.query.placedby) return res.status(401).json({ status: 401, error: 'user access denied' });
+      if (!req.userData.admin && rows[0]) {
+        if (rows[0].placedby !== req.userData.id) return res.status(401).json({ status: 401, error: 'user access denied' });
       }
       return res.status(200).json({ status: 200, data: rows });
     } catch (error) {
@@ -79,17 +70,12 @@ const Parcel = {
     }
   },
   async changeDestination(req, res) {
-    const userAccess = auth.userAuth(req);
-    const adminAccess = auth.adminAuth(req);
-    if (!(userAccess || adminAccess)) return res.status(401).json({ status: 401, error: 'user access denied' });
-
-    let text = 'SELECT * FROM parcels where id = $1';
-
     try {
       const { rows } = await db.query(text, [req.params.parcelId]);
-      // check if user id corresponds with placedby value
+      if (!rows[0]) return res.status(404).json({ status: 404, error: 'Parcel not found' });
 
-      if (req.body.placedby !== rows[0].placedby && !adminAccess) {
+      // check if user id corresponds with placedby value
+      if (req.userData.id !== rows[0].placedby && !req.userData.admin) {
         return res.status(401).json({ status: 401, error: 'user unauthorized' });
       }
       // check if parcel is yet to be delivered
@@ -97,7 +83,7 @@ const Parcel = {
       text = 'UPDATE parcels SET sentto = $1 WHERE id = $2 returning *';
       try {
         const { rows } = await db.query(text, [req.body.sentto, req.params.parcelId]);
-        mail(req.body.userEmail, rows[0].id, 'Parcel Destination change', rows[0].sentto);
+        mail(rows[0].email, rows[0].id, 'Parcel Destination change', rows[0].sentto);
         return res.status(200).json({
           status: 200,
           data: [{
@@ -115,25 +101,21 @@ const Parcel = {
   },
   // PATCH  /parcels/<parcelId>/cancel. Cancel a specific parcel delivery order.
   async cancel(req, res) {
-    const userAccess = auth.userAuth(req);
-    const adminAccess = auth.adminAuth(req);
-    if (!(userAccess || adminAccess)) return res.status(401).json({ status: 401, error: 'user access denied' });
-
-    let text = 'SELECT * FROM parcels where id = $1';
-
     try {
       const { rows } = await db.query(text, [req.params.parcelId]);
+      if (!rows[0]) return res.status(404).json({ status: 404, error: 'Parcel not found' });
+
       // check if user id corresponds with placedby value
-      if (req.body.placedby !== rows[0].placedby && !adminAccess) {
+      if (req.userData.id !== rows[0].placedby && !req.userData.admin) {
         return res.status(401).json({ status: 401, error: 'user unauthorized' });
       }
-      if (rows[0].active === 'false') return res.status(409).json({ status: 409, error: 'Parcel already cancelled' });
+      // if (rows[0].active === 'false') return res.status(409).json({ status: 409, error: 'Parcel already cancelled' });
       if (rows[0].status === 'delivered') return res.status(409).json({ status: 409, error: 'action not allowed. Parcel already delivered' });
 
       text = 'UPDATE parcels SET active = $1 WHERE id = $2 returning *';
       try {
         const { rows } = await db.query(text, [false, req.params.parcelId]);
-        mail(req.body.userEmail, rows[0].id, 'Parcel Order Cancellation', false);
+        mail(rows[0].email, rows[0].id, 'Parcel Order Cancellation', false);
         return res.status(200).json({
           status: 200,
           data: [{
@@ -153,14 +135,13 @@ const Parcel = {
   // Only the Admin is allowed to access this endpoint.
   // include user email in req.body
   async changeStatus(req, res) {
-    const access = auth.adminAuth(req); // verify it's admin trying to change status
-    if (!access) return res.status(401).json({ status: 401, error: 'user access denied' });
-
-    const text = 'UPDATE parcels SET status = $1 WHERE id = $2 returning *';
+    const update = 'UPDATE parcels SET status = $1 WHERE id = $2 returning *';
     try {
-      const { rows } = await db.query(text, [req.body.status, req.params.parcelId]);
-      if (!rows[0]) res.status(404).json({ status: 404, error: 'parcel not found' });
-      mail(req.body.userEmail, rows[0].id, 'Parcel Status change', rows[0].status);
+      await db.query(update, [req.body.status, req.params.parcelId]);
+      // fetch parcel with user email
+      const { rows } = await db.query(text, [req.params.parcelId]);
+      if (!rows[0]) return res.status(404).json({ status: 404, error: 'parcel not found' });
+      mail(rows[0].email, rows[0].id, 'Parcel Status change', rows[0].status);
       return res.status(200).json({
         status: 200,
         data: [{
@@ -177,14 +158,14 @@ const Parcel = {
   // Change the present location of a specific parcel delivery order.
   // Only the Admin is allowed to access this endpoint..
   async changeLocation(req, res) {
-    const access = auth.adminAuth(req);
-    if (!access) return res.status(401).json({ status: 401, error: 'user access denied' });
-
     const text = 'UPDATE parcels SET currentlocation = $1 WHERE id = $2 returning *';
     try {
+      await db.query(text, [req.body.currentlocation, req.params.parcelId]);
+
       const { rows } = await db.query(text, [req.body.currentlocation, req.params.parcelId]);
-      if (!rows[0]) res.status(404).json({ status: 404, error: 'parcel not found' });
-      mail(req.body.userEmail, rows[0].id, 'Parcel Current Location change', rows[0].currentlocation);
+      if (!rows[0]) return res.status(404).json({ status: 404, error: 'parcel not found' });
+
+      mail(rows[0].email, rows[0].id, 'Parcel Current Location change', rows[0].currentlocation);
       return res.status(200).json({
         status: 200,
         data: [{
