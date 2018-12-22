309fc1f69b238b400e3358413e3cfbd82499647c
je-ni <jenniferolibie@gmail.com>
2018-12-22 12:52:30 +0100

    still console.logging

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,6 @@ const { Pool } = require('pg'); // used cause the code is run from terminal
 const env = process.env.NODE_ENV || 'development';
 // eslint-disable-next-line import/no-dynamic-require
 const db = env === 'test' ? 'sendit_test' : 'sendit';
-console.log(db);
 // dotenv doesnt load here
 module.exports = () => {
   const connectionString = {
@@ -12,6 +11,7 @@ module.exports = () => {
     host: 'localhost',
   };
   const pool = new Pool(connectionString);
+  console.log(pool);
   pool.on('connect', () => console.log('connected to db'));
   return pool;
 };
