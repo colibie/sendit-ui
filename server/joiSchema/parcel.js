f2005282db3bc2b4ccaf938172a7e95e3ab4216d
je-ni <jenniferolibie@gmail.com>
2018-12-28 00:20:10 +0100

    added more tests to improve coverage

--- server/joiSchema/parcel.js
+++ server/joiSchema/parcel.js
@@ -7,7 +7,6 @@ const Schema = {
     return result.error.details[0].message;
   },
   create: Joi.object().keys({
-    placedby: Joi.string().required(),
     weight: Joi.number().required(),
     weightmetric: Joi.string().allow(''),
     sentfrom: Joi.string().required(),
