d8abd3cf101d9b8e74f4f95aed88109db83b007f
je-ni <jenniferolibie@gmail.com>
2019-01-05 22:52:00 +0100

    changed the ENV from  json to js

--- server/middleware/passwordMiddleware.js
+++ server/middleware/passwordMiddleware.js
@@ -1,5 +1,5 @@
 import crypto from 'crypto'; // Built-in encryption module
-import secrets from './ENV.json';
+import secrets from './ENV';
 
 export default (userPassword) => {
   const hashPassword = crypto.createHash('sha256').update(userPassword).digest(secrets.base);
