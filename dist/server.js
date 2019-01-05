70ea21c89b594608eefae12b545c55e7a880e1ef
je-ni <jenniferolibie@gmail.com>
2019-01-06 00:19:24 +0100

    transpiled code

--- dist/server.js
+++ dist/server.js
@@ -2,9 +2,9 @@
 
 // This will be our application entry. We'll setup our server here.
 
-var app = require('./app'); // The express app we just created
-
 require('babel-polyfill');
+
+var app = require('./app'); // The express app we just created
 
 var port = parseInt(process.env.PORT, 10) || 8000;
 
