695be644717f5844be9407eb50bf48161ff27ea1
je-ni <jenniferolibie@gmail.com>
2019-01-08 02:50:48 +0100

    set up database for production

--- server/config/database.js
+++ server/config/database.js
@@ -3,14 +3,26 @@ const secrets = require('../middleware/ENV').default;
 
 const env = process.env.NODE_ENV || 'development';
 
-const db = env === 'test' ? secrets.testDb : secrets.database;
+let connectionString = {
+  user: secrets.user,
+  database: secrets.testDb,
+  host: secrets.host,
+};
+if (env === 'test') {
+  connectionString.database = secrets.testDb;
+} else if (env === 'production') {
+  connectionString = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  };
+} else {
+  connectionString.database = secrets.database;
+}
+
+// const db = env === 'test' ? secrets.testDb :
+// env === 'production' ? process.env.DATABASE_URL : secrets.database;
 
 module.exports = () => {
-  const connectionString = {
-    user: secrets.user,
-    database: db,
-    host: secrets.host,
-  };
   const pool = new Pool(connectionString);
   pool.on('connect', () => console.log('connected to db'));
   return pool;
